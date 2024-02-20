058b9e7452b604a4ca38d909e2f1b6ddef87a5f1
ipiwixx <antoine13141@gmail.com>
2024-02-20 17:23:11 +0100

    Test 23. Edit ApplyRemiseProduct function

--- src/panier.js
+++ src/panier.js
@@ -7,30 +7,32 @@ class Panier {
 
   getMontantTotal() {
     var total = 0;
-    this.panier.forEach((produit) => total += produit['quantite'] * produit['produit'].montant);
+    this.panier.forEach(
+      (produit) => (total += produit["quantite"] * produit["produit"].montant)
+    );
     return total;
   }
 
   getNbArticle() {
     var total = 0;
-    this.panier.forEach((produit) => total += produit['quantite']);
+    this.panier.forEach((produit) => (total += produit["quantite"]));
     return total;
   }
 
   addArticle(produit, quantite) {
-    this.panier.push({produit, quantite});
+    this.panier.push({ produit, quantite });
     produit.removeStock(quantite);
   }
 
   removeArticle(produit, quantite) {
-    const index = this.panier.findIndex(item => item.produit === produit);
+    const index = this.panier.findIndex((item) => item.produit === produit);
     if (index !== -1) {
-        this.panier[index].quantite -= quantite;
-        produit.addStock(quantite);
-        if (this.panier[index].quantite <= 0) {
-            this.panier.splice(index, 1);
-            //produit.addStock(quantite);
-        }
+      this.panier[index].quantite -= quantite;
+      produit.addStock(quantite);
+      if (this.panier[index].quantite <= 0) {
+        this.panier.splice(index, 1);
+        //produit.addStock(quantite);
+      }
     }
   }
 
@@ -39,12 +41,12 @@ class Panier {
   }
 
   applyRemise(remise) {
-    if(remise.pourcentage > 0 && remise.pourcentage < 100) {
-      if (!this.remiseUsed.some(item => item === remise)) {
-          this.remiseUsed.push(remise);
-          const montantTotal = this.getMontantTotal(); // Obtenez le montant total actuel
-          const montantRemise = montantTotal * (remise.pourcentage / 100); // Calculez le montant de la remise
-          this.montantTotal = montantTotal - montantRemise; // Appliquez la remise au montant total
+    if (remise.pourcentage > 0 && remise.pourcentage < 100) {
+      if (!this.remiseUsed.some((item) => item === remise)) {
+        this.remiseUsed.push(remise);
+        const montantTotal = this.getMontantTotal(); // Obtenez le montant total actuel
+        const montantRemise = montantTotal * (remise.pourcentage / 100); // Calculez le montant de la remise
+        this.montantTotal = montantTotal - montantRemise; // Appliquez la remise au montant total
       }
     } else {
       this.montantTotal = this.getMontantTotal();
@@ -52,12 +54,14 @@ class Panier {
   }
 
   applyRemiseProduct(remise, produit, listeRemise) {
-    if(remise.pourcentage > 0 && remise.pourcentage < 100) {
-      console.log('ahah');
-      if (!produit.remiseUsed.some(item => item === remise)) {
-        console.log('ça passe');
+    if (remise.pourcentage > 0 && remise.pourcentage < 100) {
+      if (listeRemise.includes(remise)) {
+        if (!produit.remiseUsed.some((item) => item === remise)) {
           produit.remiseUsed.push(remise);
-          this.montantTotal = this.getMontantTotal() - (produit.montant * (remise.pourcentage / 100)); // Obtenez le montant total actuel
+          this.montantTotal =
+            this.getMontantTotal() -
+            produit.montant * (remise.pourcentage / 100); // Obtenez le montant total actuel
+        }
       }
     } else {
       this.montantTotal = this.getMontantTotal();
